df6325ca13f8839e4ecfd4a1efa3e3ce4dda5b24
WhiteFang <jorisschroeven@hotmail.com>
2022-05-01 21:05:19 +0200

    Improved setting merging to allow deep-object and array merging

--- src/app/layout/service/user-settings.service.ts
+++ src/app/layout/service/user-settings.service.ts
@@ -46,17 +46,12 @@ export class UserSettingsService {
 
         modules.forEach((x) => {
           const featureSettings = x.getSettings()
-          mergedSettings = {
-            ...mergedSettings,
-            ...featureSettings.defaultSettings,
-          }
+          mergedSettings = this.merge(mergedSettings, featureSettings.defaultSettings)
           this.userSettingsFeatureService.register(featureSettings)
         })
 
-        mergedSettings = {
-          ...mergedSettings,
-          ...savedSettings,
-        }
+
+        mergedSettings = this.merge(mergedSettings, savedSettings)
 
         return this.userSettingsStorageService.save(mergedSettings)
       })
@@ -73,4 +68,48 @@ export class UserSettingsService {
       map((settings) => settings as TUserSettings)
     )
   }
+
+  private merge(left: any, right: any): any {
+    return Object.keys(right).reduce((result, currentKey) => {
+      const rightValue = right[currentKey]
+      const leftValue = result[currentKey]
+      const rightValueType = typeof rightValue
+      const leftValueType = typeof leftValue
+      if (leftValue && rightValue && (leftValueType != rightValueType || Array.isArray(leftValue) != Array.isArray(rightValue))) {
+        result[currentKey] = leftValue
+        return result
+      }
+      if (rightValue && Array.isArray(rightValue)) {
+        if (leftValue) {
+          const merged = [...leftValue]
+          rightValue.forEach((value, index) => {
+            switch (typeof value) {
+              case 'object':
+                merged[index] = this.merge(leftValue[index], value)
+                break
+
+              default:
+                merged[index] = value
+                break
+            }
+          })
+          result[currentKey] = merged
+        } else {
+          result[currentKey] = rightValue
+        }
+      } else {
+        switch (rightValueType) {
+          case 'object':
+            result[currentKey] = this.merge(leftValue, rightValue)
+            break
+
+          default:
+            result[currentKey] = rightValue
+            break
+        }
+      }
+      return result
+    }, { ...left })
+  }
+
 }
