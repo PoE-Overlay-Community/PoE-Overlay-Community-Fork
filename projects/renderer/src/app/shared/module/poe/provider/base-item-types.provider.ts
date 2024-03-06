52d2d9df491a00450a65ea14c187e32518a61d0b
Jon <4leite@gmail.com>
2024-03-06 22:37:42 +1300

    add TODO for dealing with v2 duplicates

--- projects/renderer/src/app/shared/module/poe/provider/base-item-types.provider.ts
+++ projects/renderer/src/app/shared/module/poe/provider/base-item-types.provider.ts
@@ -1,7 +1,10 @@
 import { Injectable } from '@angular/core'
 import baseItemTypesArray from '../../../../../assets/poe/base-item-types-normalised.json'
+//import baseItemTypes from '../../../../../assets/poe/base-item-types-v2.json'
 import { EnumValues } from '../../../../core/class'
 import { BaseItemType, BaseItemTypeNameMap, Language } from '../type'
+
+// TODO: remove old code / revert and fix duplicate keys in json
 
 @Injectable({
   providedIn: 'root',
@@ -16,11 +19,18 @@ export class BaseItemTypesProvider {
     for (const language of languages.keys) {
       this.baseItemTypeNames[language] = {}
     }
+    /*
+    for (const key in baseItemTypes) {
+      const baseItemType = this.provideBaseItemType(key)
+    */
+    // start new code
     this.baseItemTypes = new Map()
     for (const item of baseItemTypesArray as [string,BaseItemType][]) {
       const key = crypto.randomUUID()
       const baseItemType = item[1]
       this.baseItemTypes.set(key, baseItemType)
+    // end new code
+      
       for (const language in baseItemType.names) {
         const name = baseItemType.names[language]
         if (!this.baseItemTypeNames[+language][name]) {
@@ -31,6 +41,7 @@ export class BaseItemTypesProvider {
   }
 
   public provideBaseItemType(id: string): BaseItemType {
+    // return baseItemTypes[id]	
     return this.baseItemTypes.get(id)
   }
 
